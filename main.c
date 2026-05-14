4300a694de61447da4733ffbc71db7171aaa3873
yosuk <yosukeshio@icloud.com>
2026-05-14 15:27:25 +0900

    Add month input validation

--- main.c
+++ main.c
@@ -41,6 +41,18 @@ int main(void)
 	printf("月："); scanf("%d",&m);
 	printf("日："); scanf("%d",&d);
 
+	/* 月の入力チェック */
+	if(m < 1 || m > 12){
+	printf("月の値が不正です\n");
+	return 0;
+	}
+
+	/* 日の入力チェック */
+	if(d < 1 || d > day[isleap(y)][m]){
+	printf("日の値が不正です\n");
+	return 0;
+}	
+
 	ay = by = y;
 	am = bm = m;
 	ad = bd = d;
